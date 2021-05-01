2fd47d2eb691e654be6eb5ea9afe600e959f9dc0
xternet <majewski.post@gmail.com>
2021-05-01 02:38:49 +0200

    .

--- scripts/mint_dai_via_script.js
+++ scripts/mint_dai_via_script.js
@@ -1,12 +1,12 @@
 const { ethers, Wallet, Contract } = require("ethers");
 const { legos } = require("@studydefi/money-legos");
 
-const privateKey = '0x637950a9c80a16fb645c44c0c41b9359a5e34dd3e47bb66da5c052034e8883a8' //paste Private Key of 1st ganache-cli account
+const privateKey = '0x0' //paste Private Key of 1st ganache-cli account
 const provider = new ethers.providers.JsonRpcProvider(); //connect to ganache on port 8545
 const wallet = new ethers.Wallet(privateKey, provider)
 
 module.exports = async function(callback) {
-	try{
+  try{
     const DAI = new ethers.Contract(legos.erc20.dai.address, legos.erc20.dai.abi, wallet);
 
     const proxyRegistry = new ethers.Contract(
@@ -45,7 +45,7 @@ module.exports = async function(callback) {
     ]);
   
     console.log('Balance ETH b4:', ethers.utils.formatEther(await wallet.getBalance()))
-		console.log('Balance DAI b4:', ethers.utils.formatEther(await DAI.balanceOf(wallet.address)))
+    console.log('Balance DAI b4:', ethers.utils.formatEther(await DAI.balanceOf(wallet.address)))
   
     // Open vault through proxy
     await proxyContract.execute(legos.maker.dssProxyActions.address, _data, {
@@ -54,8 +54,8 @@ module.exports = async function(callback) {
     });
 
     console.log('Balance ETH after:', ethers.utils.formatEther(await wallet.getBalance()))
-		console.log('Balance DAI after:', ethers.utils.formatEther(await DAI.balanceOf(wallet.address)))
-	} catch (e) {
-		console.log(e)
-	} callback()
+    console.log('Balance DAI after:', ethers.utils.formatEther(await DAI.balanceOf(wallet.address)))
+  } catch (e) {
+    console.log(e)
+  } callback()
 }
